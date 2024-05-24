bff7e7b0988aee5b438bb469a667b27cb219e71e
Kikyou726 <Kikyoumusashi1997@outlook.com>
2024-05-24 09:58:35 +0800

    fj

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(s); // 显示 hello
+        s = "future";
+        System.out.println(s); // 显示 world
+    }
+}
