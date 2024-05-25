35329903ed592b14bc1f5ae29a2b833789ab18d9
Kikyou726 <Kikyoumusashi1997@outlook.com>
2024-05-25 09:16:55 +0800

    fj

---
+++ Main.java
@@ -0,0 +1,23 @@
+<plugin>
+<groupId>org.apache.maven.plugins</groupId>
+<artifactId>maven-shade-plugin</artifactId>
+<configuration>
+<minimizeJar>true</minimizeJar>
+<filters>
+<filter>
+<artifact>*:*</artifact>
+<excludes>
+<exclude>META-INF/**</exclude>
+ </excludes>
+ </filter>
+ </filters>
+ </configuration>
+ <executions>
+ <execution>
+ <phase>package</phase>
+ <goals>
+ <goal>shade</goal>
+ </goals>
+ </execution>
+ </executions>
+ </plugin>
