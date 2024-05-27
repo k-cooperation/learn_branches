26ddce883e8023deeaa4d7f2b7a43b96c76db06a
Kikyou726 <Kikyou726@Kikyoumusashi1997@outlook.com>
2024-05-27 21:39:17 +0800

    zaijia

---
+++ complie.java
@@ -0,0 +1,8 @@
+public class complie {
+    public static void main(String[] args) {
+        double i =50.0;
+        double k =50.0 + i;
+        double j =k + 1;
+        System.out.println("j is "+ j  + " and k is " + k);
+    }
+}
