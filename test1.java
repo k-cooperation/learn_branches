c84c36686742c9e8a6182cf382479477ff2dbb77
Kikyou726 <Kikyou726@Kikyoumusashi1997@outlook.com>
2024-05-28 23:13:15 +0800

    first_java_from_Kikyou

---
+++ test1.java
@@ -0,0 +1,10 @@
+public class test1 {
+    public static void main(String[] args) {
+        int  x = 10;
+        while(x < 20){
+           System.out.print(" value of x is " + x);
+           x++;
+           System.out.print("\n" );
+        }
+    }
+}
