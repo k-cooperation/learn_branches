1b6fe03e4065402d1105a72ee7e06bea463ff778
Kikyou726 <Kikyou726@Kikyoumusashi1997@outlook.com>
2024-05-28 23:24:58 +0800

    first_java_push_from_Kikyou726

--- test1.java
+++ test1.java
@@ -1,7 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         int  x = 10;
-        while(x < 20){
+        while(x < 21){
            System.out.print(" value of x is " + x);
            x++;
            System.out.print("\n" );
