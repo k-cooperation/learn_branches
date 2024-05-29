cfa8d3e016b6e4de445117e5c9a77d37c79e04b7
Kikyou726 <Kikyoumusashi1997@outlook.com>
2024-05-29 10:46:35 +0800

    test_java_from_huajian

---
+++ test1.java
@@ -0,0 +1,9 @@
+public class test1 {
+    public static void main(String[] args) {
+        System.out.print("A,");
+        System.out.print("B,");
+        System.out.print("C");
+        System.out.println( );
+        System.out.println("END");
+    }
+}
