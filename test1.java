aceb6cc334c3d7fd5548c6299e5e2e8c2e48ea61
Kikyou726 <Kikyou726@Kikyoumusashi1997@outlook.com>
2024-05-29 22:43:41 +0800

    processing

--- test1.java
+++ test1.java
@@ -1,15 +1,15 @@
 public class test1 {
     public static void main(String[] args) {
-        int  x = 10;
-        while(x < 21){
-           System.out.print(" value of x is " + x);
-           x++;
-           System.out.print("\n" );
-       
-        System.out.print("A,");
-        System.out.print("B,");
-        System.out.print("C++");
-        System.out.println( );
-        System.out.println("END");
+        int x = 10;
+        while (x < 21) {
+            System.out.print(" value of x is " + x);
+            x++;
+            System.out.print("\n");
+            System.out.print("A,");
+            System.out.print("B,");
+            System.out.print("C++");
+            System.out.println();
+            System.out.println("END");
+        }
     }
 }
