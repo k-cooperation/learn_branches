77e3694fb54fb3baf309a35d863404b44f10cd3c
Kikyou726 <Kikyou726@Kikyoumusashi1997@outlook.com>
2024-05-29 21:57:57 +0800

    if qiantao

---
+++ Qiantao.java
@@ -0,0 +1,11 @@
+public class Qiantao{
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 10;
+        if(x == 30) {
+            if (y == 10) {
+                System.out.print("x = 30 y = 10");
+            }
+        }
+    }
+}
