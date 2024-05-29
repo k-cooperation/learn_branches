4ccb1e9c8b73c6c46d6a6e2100f86b823412b28a
Kikyou726 <Kikyoumusashi1997@outlook.com>
2024-05-29 11:09:03 +0800

    remrege

--- test1.java
+++ test1.java
@@ -2,7 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.print("A,");
         System.out.print("B,");
-        System.out.print("C");
+        System.out.print("C++");
         System.out.println( );
         System.out.println("END");
     }
