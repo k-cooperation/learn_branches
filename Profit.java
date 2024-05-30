cf6e07ee3b4ae2c672403e99a2e06f64f6191576
Kikyou726 <Kikyoumusashi1997@outlook.com>
2024-05-30 09:53:46 +0800

    profit

---
+++ Profit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Profit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("plz Enter the I number"    );
+        double I = 0;
+        if(I < 10){
+
+        }
+    }
+}
