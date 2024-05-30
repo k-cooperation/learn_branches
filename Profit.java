e0981208ec3a171aecfe6bc6e78e1a3d549d742d
Kikyou726 <Kikyoumusashi1997@outlook.com>
2024-05-30 09:54:54 +0800

    profit

--- Profit.java
+++ Profit.java
@@ -3,9 +3,20 @@ public class Profit {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("plz Enter the I number"    );
-        double I = 0;
-        if(I < 10){
-
-        }
+        double P = input.nextDouble();
+        double B;
+        B = 0.0;
+        if(P <= 10){
+            System.out.print(B = P * 0.1);
+        } else if (P <= 20) {
+            System.out.print(B = 0.1 * (P - 10) + 0.075 * P - (P - 10) );
+        } else if (P <= 40) {
+            System.out.print(B = 0.05 * (P - (P - 20)));
+        } else if (P <= 60) {
+            System.out.print(B = 0.03 * (P - (P - 40)));
+        } else if (P < 100){
+            System.out.print(B = 0.015 * (P -(P - 60)));
+        } else if (P > 100)
+            System.out.print(B = 0.01 * (P -(P - 100)));
     }
 }
