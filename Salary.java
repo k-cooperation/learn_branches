3afc2181777ce0c6c120946d60c1808bafc1994c
跨越七海的风 <129412954+Kikyou726@users.noreply.github.com>
2024-06-04 22:42:44 +0800

    Update Salary.java
    test for salsry

--- Salary.java
+++ Salary.java
@@ -1,7 +1,7 @@
 public class Salary {
     public static void main(String[] args) {
         String name = "Kikyou";
-        int year = 1;
+        int year = 100;
         double salary =10000.0;
         System.out.println("hello there this is Kikyou "+"this is the " + year + "th year that i study Java "+" and the salary i desire is "+ salary);
     }
