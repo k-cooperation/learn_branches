f115de550b078a93dacb10b4e9b144b653b454b6
Kikyou726 <Kikyou726@Kikyoumusashi1997@outlook.com>
2024-06-04 22:33:46 +0800

    salary

--- Salary.java
+++ Salary.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Salary {
+public class Salary {
+    public static void main(String[] args) {
+        String name = "Kikyou";
+        int year = 1;
+        double salary =10000.0;
+        System.out.println("hello there this is Kikyou "+"this is the " + year + "th year that i study Java "+" and the salary i desire is "+ salary);
+    }
 }
