14995ea440e244378c1766ab6bda6297e95357ef
Kikyou726 <Kikyou726@Kikyoumusashi1997@outlook.com>
2024-06-27 22:02:48 +0800

    Area calcualte

---
+++ AreaCalculate.java
@@ -0,0 +1,16 @@
+public class AreaCalculate {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double r1 = 2.6;
+        double r2 = 3.5;
+        int r3 = 6;
+
+        double area1 = pi * r1 * r1;
+        double area2 = pi * r2 * r2;
+        double area3 = pi * r3 * r3;
+
+        System.out.println("area1 is " + area1 );
+        System.out.println("area2 is " + area2 );
+        System.out.println("area3 is " + area3 );
+    }
+}
---
+++ GuessNumberGame.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumberGame {
+    public static void main(String[] args) {
+        // 创建一个随机数生成器
+        Random rand = new Random();
+        // 生成一个 1 到 100 之间的随机数
+        int numberToGuess = rand.nextInt(100) + 1;
+        int numberOfTries = 0;
+        Scanner input = new Scanner(System.in);
+        int guess;
+        boolean guessed = false;
+
+        System.out.println("欢迎来到猜数字游戏！");
+        System.out.println("我已经选择了一个 1 到 100 之间的数字。");
+
+        while (!guessed) {
+            System.out.print("请输入你的猜测：");
+            guess = input.nextInt();
+            numberOfTries++;
+
+            if (guess < numberToGuess) {
+                System.out.println("太小了！再试一次。");
+            } else if (guess > numberToGuess) {
+                System.out.println("太大了！再试一次。");
+            } else {
+                System.out.println("恭喜你，猜中了！这个数字是 " + numberToGuess + "。");
+                System.out.println("你总共猜了 " + numberOfTries + " 次。");
+                guessed = true;
+            }
+        }
+
+        // 关闭输入流
+        input.close();
+    }
+}
